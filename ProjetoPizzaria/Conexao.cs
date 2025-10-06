3f320b2daf8d5c2745fea1408ebe0fee138aa9e2
Luísa <luevangelista0@gmail.com>
2025-10-06 20:10:32 -0300

    Criando a string de conexão

---
+++ ProjetoPizzaria/Conexao.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPizzaria
+{
+    class Conexao
+    {
+        //INSTANCIANDO MYSQL
+        MySqlConnection con = new MySqlConnection("Data Source=localhost;Initial Catalog=dbpizza;user=root;pwd=12345678");
+
+        public static string msg;
+
+        public MySqlConnection ConnectarBD()
+        {
+            try
+            {
+                con.Open();
+
+            }
+            catch (Exception erro)
+            {
+
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return con;
+        }
+
+        public MySqlConnection DesConnectarBD()
+        {
+            try
+            {
+                con.Close();
+
+            }
+            catch (Exception erro)
+            {
+
+                msg = "Ocorreu um erro ao se conectar" + erro.Message;
+            }
+            return con;
+        }
+    }
+}
